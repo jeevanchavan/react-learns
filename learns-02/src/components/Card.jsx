b00c924864216c6711c01dbb59acc319370779a8
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-12 17:12:37 +0530

    session2 practice on components

---
+++ learns-02/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='Card'>
+        <h1>Jeevan Chavan</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, quae.</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ learns-02/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <h3>Home!</h3>
+    </div>
+  )
+}
+
+export default Navbar
