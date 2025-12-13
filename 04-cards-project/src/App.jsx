d5fe17a01c6ab235bf6b8627e804ab6d6acf972d
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-14 01:02:04 +0530

    mini card project 04 section html structure done

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+
+        <div className="card">
+          <div className="top">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png" alt="" />
+              <button>Save <i class="ri-save-fill"></i></button>
+          </div>
+          {/* <div className="center">
+            <h3>Amazon <span>5 days ago</span></h3>
+            <h2>Senior UI/UX Desiner</h2>
+            <div>
+              <h4>Part-Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+          <div className="bottom">
+            <div>
+              <h3>$120/hr</h3>
+              <p>Mumbai,India</p>
+            </div>
+            <button>Apply now</button>
+          </div> */}
+        </div>
+
+    </div>
+  )
+}
+
+export default App
