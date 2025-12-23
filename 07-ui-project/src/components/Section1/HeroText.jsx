6b0a4ec1c20534a925e185d5451d2f077224e4a7
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-24 00:14:58 +0530

    half project done..section1 navbar and left page content

---
+++ 07-ui-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-5'>
+            <h3 className='text-6xl font-bold'>Prospective <br /> customer <br /> segmentation</h3>
+            <p className='mt-7 text-gray-500 font-medium text-xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur pariatur tenetur quae accusantium nam velit ex cupiditate vel blanditiis totam.</p>
+        </div>
+  )
+}
+
+export default HeroText
