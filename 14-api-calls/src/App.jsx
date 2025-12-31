3a8cf75dd6271839813fb6e5c3fbb25ff2424948
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-31 22:31:37 +0530

    api calling using axiosused state done

--- 14-api-calls/src/App.jsx
+++ 14-api-calls/src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 const App = () => {
 
@@ -13,15 +15,29 @@ const App = () => {
   //     .catch(err => console.log(err));
   // }
 
+  // const getData = async ()=>{
+  //   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  //   const data = await res.json();
+  //   console.log(data);
+  // }
+
+  const [data, setData] = useState([])
+
   const getData = async ()=>{
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const data = await res.json();
-    console.log(data);
+    const response = await axios.get('https://picsum.photos/v2/list')
+
+    setData(response.data);
   }
 
   return (
     <div>
       <button onClick={getData}>GetData</button>
+
+      <div>
+        {data.map((elem,idx)=>{
+          return <h1>Hello {elem.author} {idx}</h1>
+        })}
+      </div>
     </div>
   )
 }
