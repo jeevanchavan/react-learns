3134352039b8e570d50be2ff430c21150efbee40
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-31 18:13:13 +0530

    project completed at last made different file for each componenet

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -26,6 +26,13 @@ const App = () => {
     setUserDesc('')
   } 
 
+  const deleteHandler =(idx)=>{
+    const copyUsers = [...allUsers];
+    copyUsers.splice(idx,1);
+
+    setAllUsers(copyUsers);
+  }
+
   return (
     <div className='bg-black h-screen text-white'>
       <form onSubmit={(e)=>{
@@ -75,13 +82,7 @@ const App = () => {
       <div className='flex p-5 flex-wrap gap-5'>
 
         {allUsers.map(function(elem,idx){
-            return  < div key={idx} className=' lg:w-[22vw] md:w-[30vw] sm:w-[45vw] px-8 py-8 flex  items-center flex-col rounded-2xl bg-white text-black'>
-              <img className='w-25 h-25 rounded-4xl object-cover object-center' src={elem.imageURL} alt="" />
-              <h1 className='font-bold text-black text-3xl mt-3'>{elem.userName}</h1>
-              <h3 className='text-green-600 text-2xl font-semibold'>{elem.userRole}</h3>
-              <p className='leading-tight text-sm font-medium mt-5 align-middlelg: '>{elem.userDesc}</p>
-              <button className='rounded bg-red-500 px-8 py-2 text-white mt-5 font-semibold cursor-pointer active:scale-95 '>Remove</button>
-          </div>
+            return  <Card key={idx} idx={idx} elem={elem} deleteHandler={deleteHandler}/>
         })}
 
     </div>
--- 12-notes-app/src/components/Card.jsx
+++ 12-notes-app/src/components/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className='w-[22vw] px-8 py-8 flex items-center flex-col rounded-2xl bg-white text-black'>
-        <img className='w-25 h-25 rounded-4xl object-cover' src="https://i.pinimg.com/736x/df/35/b7/df35b7ec42a710f17fde998c7b5d8f5b.jpg" alt="" />
-        <h1 className='font-bold text-black text-3xl mt-3'>Jeevan Chavan</h1>
-        <h3 className='text-green-600 text-2xl font-semibold'>Developer</h3>
-        <p className='leading-tight text-sm font-medium mt-5 '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, temporibus.</p>
-        <button className='rounded bg-red-500 px-8 py-2 text-white mt-5 font-semibold cursor-pointer active:scale-95 '>Remove</button>
-    </div>
+    < div key={props.idx} className=' lg:w-[22vw] md:w-[30vw] sm:w-[45vw] px-8 py-8 flex  items-center flex-col rounded-2xl bg-white text-black'>
+              <img className='w-25 h-25 rounded-4xl object-cover object-center' src={props.elem.imageURL} alt="" />
+              <h1 className='font-bold text-black text-3xl mt-3'>{props.elem.userName}</h1>
+              <h3 className='text-green-600 text-2xl font-semibold'>{props.elem.userRole}</h3>
+              <p className='leading-tight text-sm font-medium mt-5 align-middlelg: '>{props.elem.userDesc}</p>
+              <button onClick={()=>{
+                props.deleteHandler(props.idx);
+              }} className='rounded bg-red-500 px-8 py-2 text-white mt-5 font-semibold cursor-pointer active:scale-95 '>Remove</button>
+          </div>
   )
 }
 
