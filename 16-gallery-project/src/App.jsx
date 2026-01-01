e739253da347b27757d8dcdf3ad72e74ebb35948
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-02 00:18:43 +0530

    first overview of gallery project is done; used use effect to get data without button

--- 16-gallery-project/src/App.jsx
+++ 16-gallery-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
@@ -12,24 +12,32 @@ const App = () => {
 
   }
 
-  let printUserData = 'No user Availabe'
+  useEffect(function(){
+    getData()
+  },[])
+
+  let printUserData = <h3 className='text-gray-600 font-medium'>No user Available</h3>
 
   if(userData.length>0){
-    printUserData = userData.map(function(elem){
+    printUserData = userData.map(function(elem,idx){
 
-      return <div >
-        <div className='h-40 w-45 bg-white object-cover'>
-          <img className='h-40' src={elem.download_url} alt="" />
+      return <div key={idx}>
+        <a href={elem.url} target="_">
+          <div className='h-48 w-48 overflow-hidden rounded-xl'>
+          <img className='h-full w-full object-cover' src={elem.download_url} alt="" />
         </div>
+
+        <h2 className='font-bold text-lg'>{elem.author}</h2>
+        </a>
       </div>
     })
   }
 
   return (
-    <div className='bg-black overflow-auto h-screen text-white p-4'>
-      <button onClick={getData} className='bg-green-600 px-8 py-5 rounded active:scale-95 cursor-pointer font-bold mb-3'>Get data</button>
+    <div className='bg-black overflow-auto h-screen w-screen text-white p-4'>
+      {/* <button onClick={getData} className='bg-green-600 px-8 py-5 rounded active:scale-95 cursor-pointer font-bold mb-3'>Get data</button> */}
 
-      <div className='flex flex-wrap gap-2'>
+      <div className='flex flex-wrap gap-3'>
         {printUserData}
       </div>
     </div>
